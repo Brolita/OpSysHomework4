951ae106cba967220c8e76db035b182c971efb08
Spencer <johnss18@rpi.edu>
2014-12-07 18:00:33 -0500

    hey look theres no garbage files to push

--- Process.py
+++ Process.py
@@ -2,7 +2,18 @@ class Process:
 	def __init__(self, _id, _frames, _eventTimes ):
 		self.id = id
 		self.frames = _frames
-		#a list of lists ex: [[arrival, exit],[arrival,exit]]
-		self.eventTimes = _eventTimes
+		
+		self.arrivalTimes = []
+		self.exitTimes = []
+		
+		#parse _eventTimes into arrival and exitTimes
+		i=0
+		for int in _eventTimes:
+			if i%2 = 0:
+				arrivalTimes.append(int)
+			else:
+				exitTimes.append(int)
+			i+= 1
+		
 		
 	
--- main.py
+++ main.py
@@ -7,6 +7,20 @@
 import sys
 import os
 
+allProcesses = []
+#time in milliseconds
+t = 0
+
+def parseFile():
+	file = sys.argv[1]
+	
+	for line in file:
+		args = line.split()
+		name = args.pop()
+		numOfFrames = args.pop()
+		newPro = Process(name, numOfFrames, args)
+		allProcesses.append(newPro)
+		
 def main():
 	mode = -1;
 	modes = ["first", "best", "next", "worst", "noncontig"]
@@ -28,3 +42,20 @@ def main():
 	if not os.path.isfile(sys.argv[len(sys.argv) - 2]):
 		print "ERROR: File not found"
 		sys.exit(0)
+	
+	#read through the file in argv[1] and make new processes which are added to allProcesses
+	parseFile()
+	
+	while true:
+		#check against all processes that need to exit
+		
+		#check against all processes that need to arrive
+		
+		#check for user input
+		
+		#increment time
+		t+= 0
+	
+
+		
+		

ef49964096a6bb5f3320e7e29e9e4e66b9c99096
Spencer <johnss18@rpi.edu>
2014-12-06 22:40:08 -0500

    process butts

---
+++ Process.py
@@ -0,0 +1,8 @@
+class Process:
+	def __init__(self, _id, _frames, _eventTimes ):
+		self.id = id
+		self.frames = _frames
+		#a list of lists ex: [[arrival, exit],[arrival,exit]]
+		self.eventTimes = _eventTimes
+		
+	
